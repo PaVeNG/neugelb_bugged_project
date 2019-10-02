6d3bf423ac175428f02a0b2ef8ca80a11f341435
Paul Vetter <paul@neugelb.com>
2019-10-02 15:07:25 +0200

    updated api key

--- app/src/main/java/com/bugged/themoviedb/util/ConstantUtils.java
+++ app/src/main/java/com/bugged/themoviedb/util/ConstantUtils.java
@@ -3,6 +3,7 @@ package com.bugged.themoviedb.util;
 
 public class ConstantUtils {
     public static final String HOST_NAME = "http://api.themoviedb.org/3/discover/";
-    public static final String HOST_NAME1 = "http://api.themoviedb.org/3/discover";
-    public static final String API_KEY = "a0939230b06e8d4868584b0d0502c148";
+    public static final String HOST_NAME_IMAGE = "http://api.themoviedb.org/3/discover";
+    public static final String IMAGE_URL = "http://image.tmdb.org/t/p/w200";
+    public static final String API_KEY = "9ef6aea77058f9a38546076d5a59d974";
 }
